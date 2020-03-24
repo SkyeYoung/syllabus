bee52824f31b28635cce6c57b124c121b3f082c3
iskye <iskyex@outlook.com>
2020-03-25 02:02:39 +0800

    Fix: BaseWidget;

--- pyqt.py
+++ pyqt.py
@@ -6,7 +6,7 @@ __author__ = 'iskye'
 import sys
 
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QDesktopWidget, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow
 
 
 class BaseWindow(QMainWindow):
@@ -20,23 +20,12 @@ class BaseWindow(QMainWindow):
         # 设置高度
         self.setFixedHeight(25)
 
-    def move_center(self):
-        """窗口居中"""
-
-        # 屏幕几何信息
-        screen = QDesktopWidget().geometry()
-        # 窗口几何信息
-        window = self.geometry()
-        # 移动窗口
-        self.move((screen.width() - window.width()) / 2, (screen.height() - window.height()) / 2)
-
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     main = BaseWindow()
     main.setWindowTitle(' ')
-    
+
     main.resize(400, 200)
-    main.move_center()
     main.show()
     sys.exit(app.exec_())
--- qt_source/BaseWidget.py
+++ qt_source/BaseWidget.py
@@ -4,32 +4,34 @@
 __author__ = 'iskye'
 
 import sys
+from ctypes import windll
 
+from PyQt5 import QtGui
 from PyQt5.QtCore import Qt, QSize, QRect
 from PyQt5.QtGui import QIcon, QPixmap, QPainter
 from PyQt5.QtWidgets import QWidget, QPushButton, QHBoxLayout, QApplication, QMainWindow, QVBoxLayout
 
-from qt_source.loadqss import load_qss
+from qt_source.LoadQSS import load_qss
 
 BAR_HEIGHT = 24  # 标题高度
 BTN_WIDTH = 24  # 按钮宽度
 BTN_HEIGHT = 21  # 按钮高度
 LABEL_SIZE = 24  # 标签尺寸
-SHADOW_WIDTH = 10
+SHADOW_SIZE = 8  # 阴影尺寸
 
 
 class TitleBar(QWidget):
-    """自定义标题栏组件，不含图标和标题"""
+    """自定义标题栏组件，不直接使用，不含图标和标题"""
 
     def __init__(self, parent):
         super(TitleBar, self).__init__()
 
         # 开始位置
-        self.start_pos = None
+        self.__start_pos = None
         # 顶层的窗口
-        self.win = parent
+        self.__win = parent
         # 是否被按下
-        self.isPressed = False
+        self.__isPressed = False
 
         """引入样式"""
         self.setAttribute(Qt.WA_StyledBackground, True)  # 设置这个才能设置 QWidget 的背景色
@@ -37,121 +39,126 @@ class TitleBar(QWidget):
 
         """三大按钮"""
         # 初始化三大按钮
-        self.min_btn = QPushButton(self)
-        self.max_btn = QPushButton(self)
-        self.close_btn = QPushButton(self)
+        self.__min_btn = QPushButton(self)
+        self.__max_btn = QPushButton(self)
+        self.__close_btn = QPushButton(self)
 
         # 设置三大按钮尺寸
-        self.min_btn.setFixedSize(BTN_WIDTH, BTN_HEIGHT)
-        self.max_btn.setFixedSize(BTN_WIDTH, BTN_HEIGHT)
-        self.close_btn.setFixedSize(BTN_WIDTH, BTN_HEIGHT)
+        self.__min_btn.setFixedSize(BTN_WIDTH, BTN_HEIGHT)
+        self.__max_btn.setFixedSize(BTN_WIDTH, BTN_HEIGHT)
+        self.__close_btn.setFixedSize(BTN_WIDTH, BTN_HEIGHT)
 
         # 设置三大按钮图标
-        self.min_btn.setIcon(QIcon('images/icon/minimize.png'))
-        self.max_btn.setIcon(QIcon('images/icon/maximize.png'))
+        self.__min_btn.setIcon(QIcon('images/icon/minimize.png'))
+        self.__max_btn.setIcon(QIcon('images/icon/maximize.png'))
 
         # 关闭按钮额外设置
-        self.close_btn.setStyleSheet(
+        self.__close_btn.setStyleSheet(
             # 鼠标未悬浮状态下
             'QPushButton{border-image:url(images/icon/close.png);}' +
             # 鼠标悬浮状态下
             'QPushButton:hover{background: #e81123; border-image:url(images/icon/close_white.png);}')
 
         # 设置三大按钮图标大小
-        self.min_btn.setIconSize(QSize(24, 21))
-        self.max_btn.setIconSize(QSize(24, 21))
-        self.close_btn.setIconSize(QSize(24, 21))
+        self.__min_btn.setIconSize(QSize(24, 21))
+        self.__max_btn.setIconSize(QSize(24, 21))
+        self.__close_btn.setIconSize(QSize(24, 21))
 
         # 设置三大按钮点击事件
-        self.min_btn.clicked.connect(self.show_minimized_window)
-        self.max_btn.clicked.connect(self.show_maximized_window)
-        self.close_btn.clicked.connect(self.close_window)
+        self.__min_btn.clicked.connect(self.__minimize_window)
+        self.__max_btn.clicked.connect(self.__maximize_window)
+        self.__close_btn.clicked.connect(self.__close_window)
 
         """布局"""
-        self.layout = QHBoxLayout(self)
-        self.setLayout(self.layout)
+        layout = QHBoxLayout(self)
+        self.setLayout(layout)
+
         # 布局样式
-        self.layout.setSpacing(0)
-        self.layout.setContentsMargins(0, 0, 0, 0)
-        self.layout.setAlignment(Qt.AlignRight)
+        layout.setSpacing(0)
+        layout.setContentsMargins(0, 0, 0, 0)
+        layout.setAlignment(Qt.AlignRight)
 
         # 添加三大按钮到标题栏
-        self.layout.addWidget(self.min_btn)
-        self.layout.addWidget(self.max_btn)
-        self.layout.addWidget(self.close_btn)
+        layout.addWidget(self.__min_btn)
+        layout.addWidget(self.__max_btn)
+        layout.addWidget(self.__close_btn)
 
-    def show_minimized_window(self):
-        self.win.showMinimized()
+    def __minimize_window(self):
+        """最小化窗口"""
+        self.__win.showMinimized()
 
-    def show_maximized_window(self):
-        if self.win.isMaximized():
-            self.win.showNormal()
+    def __maximize_window(self):
+        """最大化窗口"""
+        if self.__win.isMaximized():
+            self.__win.showNormal()
             # 调整为全屏图标
-            self.max_btn.setIcon(QIcon('images/icon/maximize.png'))
+            self.__max_btn.setIcon(QIcon('images/icon/maximize.png'))
         else:
-            self.win.showMaximized()
+            self.__win.showMaximized()
             # 调整为重置图标
-            self.max_btn.setIcon(QIcon('images/icon/restore.png'))
+            self.__max_btn.setIcon(QIcon('images/icon/restore.png'))
 
-    def close_window(self):
-        self.win.close()
+    def __close_window(self):
+        """关闭窗口"""
+        self.__win.close()
 
     def mouseDoubleClickEvent(self, event):
-        self.show_maximized_window()
+        """重写，鼠标双击事件"""
+        self.__close_window()
         return QWidget().mouseDoubleClickEvent(event)
 
     def mousePressEvent(self, event):
-        self.isPressed = True
-        self.start_pos = event.globalPos()
+        """重写，鼠标单击事件"""
+        self.__isPressed = True
+        self.__start_pos = event.globalPos()
         return QWidget().mousePressEvent(event)
 
     def mouseReleaseEvent(self, event):
-        self.isPressed = False
+        """重写，鼠标释放事件"""
+        self.__isPressed = False
         return QWidget().mouseReleaseEvent(event)
 
     def mouseMoveEvent(self, event):
-        if self.isPressed:
-            if self.win.isMaximized:
-                self.win.showNormal()
+        """重写，鼠标移动事件"""
+        if self.__isPressed:
+            if self.__win.isMaximized:
+                self.__win.showNormal()
 
-            move_pos = event.globalPos() - self.start_pos
-            self.start_pos = event.globalPos()
-            self.win.move(self.win.pos() + move_pos)
+            move_pos = event.globalPos() - self.__start_pos
+            self.__start_pos = event.globalPos()
+            self.__win.move(self.__win.pos() + move_pos)
 
         return QWidget().mouseMoveEvent(event)
 
 
-class BaseWindow(QMainWindow):
-    def __init__(self):
-        super(BaseWindow, self).__init__()
+class RawWindow(QMainWindow):
+    """自定义窗体，不直接使用"""
+
+    def __init__(self, body):
+        super(RawWindow, self).__init__()
+
+        # 真正用于存放组件的窗体
+        self.__body = body
 
         """引入样式"""
-        self.setAttribute(Qt.WA_TranslucentBackground)  # 窗口背景透明
+        self.setWindowFlags(Qt.FramelessWindowHint)  # 无框架窗体，方便使用自定义标题栏
+        self.setAttribute(Qt.WA_TranslucentBackground)  # 窗口背景透明，方便设置阴影
         self.setStyleSheet(load_qss('./style/basewindow.qss'))
 
-        self.lay = QVBoxLayout(self)
-        self.center = QWidget(self)
-        self.client = QWidget(self)
-        self.InitializeWindow()
+        """布局设置"""
+        layout = QVBoxLayout(self)  # 垂直布局
+        q_self = QWidget(self)  # 将本身转化为 QWidget
 
-    def InitializeWindow(self):
-        self.setWindowFlags(Qt.FramelessWindowHint)
-        self.InitializeViews()
+        self.setCentralWidget(q_self)
+        q_self.setLayout(layout)
+        layout.addWidget(TitleBar(self))
+        layout.addWidget(self.__body)
+        layout.setStretch(1, 100)
+        layout.setSpacing(0)
+        layout.setContentsMargins(SHADOW_SIZE, SHADOW_SIZE, SHADOW_SIZE, SHADOW_SIZE)
 
-    def InitializeViews(self):
-        self.setCentralWidget(self.center)
-
-        self.center.setLayout(self.lay)
-
-        title_bar = TitleBar(self)
-        self.lay.addWidget(title_bar)
-        self.lay.addWidget(self.client)
-        self.lay.setStretch(1, 100)
-        self.lay.setSpacing(0)
-        self.lay.setContentsMargins(0, 0, 0, 0)
-
-    def draw_shadow(self, painter):
-        # 绘制左上角、左下角、右上角、右下角、上、下、左、右边框
+    def __draw_shadow(self, painter):
+        """绘制左上角、左下角、右上角、右下角、上、下、左、右阴影"""
         pixmaps = list()
         pixmaps.append('images/shadow/left_top.png')
         pixmaps.append('images/shadow/left_bottom.png')
@@ -163,40 +170,69 @@ class BaseWindow(QMainWindow):
         pixmaps.append('images/shadow/right_mid.png')
 
         # 左上角
-        painter.drawPixmap(0, 0, SHADOW_WIDTH, SHADOW_WIDTH, QPixmap(pixmaps[0]))
+        painter.drawPixmap(0, 0, SHADOW_SIZE, SHADOW_SIZE, QPixmap(pixmaps[0]))
         # 右上角
-        painter.drawPixmap(self.width() - SHADOW_WIDTH, 0, SHADOW_WIDTH, SHADOW_WIDTH,
+        painter.drawPixmap(self.width() - SHADOW_SIZE, 0, SHADOW_SIZE, SHADOW_SIZE,
                            QPixmap(pixmaps[2]))
         # 左下角
-        painter.drawPixmap(0, self.height() - SHADOW_WIDTH, SHADOW_WIDTH, SHADOW_WIDTH,
+        painter.drawPixmap(0, self.height() - SHADOW_SIZE, SHADOW_SIZE, SHADOW_SIZE,
                            QPixmap(pixmaps[1]))
         # 右下角
-        painter.drawPixmap(self.width() - SHADOW_WIDTH, self.height() - SHADOW_WIDTH, SHADOW_WIDTH,
-                           SHADOW_WIDTH, QPixmap(pixmaps[3]))
+        painter.drawPixmap(self.width() - SHADOW_SIZE, self.height() - SHADOW_SIZE, SHADOW_SIZE,
+                           SHADOW_SIZE, QPixmap(pixmaps[3]))
         # 左
-        painter.drawPixmap(0, SHADOW_WIDTH, SHADOW_WIDTH, self.height() - 2 * SHADOW_WIDTH,
-                           QPixmap(pixmaps[6]).scaled(SHADOW_WIDTH, self.height() - 2 * SHADOW_WIDTH))
+        painter.drawPixmap(0, SHADOW_SIZE, SHADOW_SIZE, self.height() - 2 * SHADOW_SIZE,
+                           QPixmap(pixmaps[6]).scaled(SHADOW_SIZE, self.height() - 2 * SHADOW_SIZE))
         # 右
-        painter.drawPixmap(self.width() - SHADOW_WIDTH, SHADOW_WIDTH, SHADOW_WIDTH, self.height() - 2 * SHADOW_WIDTH,
-                           QPixmap(pixmaps[7]).scaled(SHADOW_WIDTH, self.height() - 2 * SHADOW_WIDTH))
+        painter.drawPixmap(self.width() - SHADOW_SIZE, SHADOW_SIZE, SHADOW_SIZE, self.height() - 2 * SHADOW_SIZE,
+                           QPixmap(pixmaps[7]).scaled(SHADOW_SIZE, self.height() - 2 * SHADOW_SIZE))
         # 上
-        painter.drawPixmap(SHADOW_WIDTH, 0, self.width() - 2 * SHADOW_WIDTH, SHADOW_WIDTH,
-                           QPixmap(pixmaps[4]).scaled(self.width() - 2 * SHADOW_WIDTH, SHADOW_WIDTH))
+        painter.drawPixmap(SHADOW_SIZE, 0, self.width() - 2 * SHADOW_SIZE, SHADOW_SIZE,
+                           QPixmap(pixmaps[4]).scaled(self.width() - 2 * SHADOW_SIZE, SHADOW_SIZE))
         # 下
-        painter.drawPixmap(SHADOW_WIDTH, self.height() - SHADOW_WIDTH, self.width() - 2 * SHADOW_WIDTH,
-                           SHADOW_WIDTH, QPixmap(pixmaps[5]).scaled(self.width() - 2 * SHADOW_WIDTH, SHADOW_WIDTH))
+        painter.drawPixmap(SHADOW_SIZE, self.height() - SHADOW_SIZE, self.width() - 2 * SHADOW_SIZE,
+                           SHADOW_SIZE, QPixmap(pixmaps[5]).scaled(self.width() - 2 * SHADOW_SIZE, SHADOW_SIZE))
 
     def paintEvent(self, event):
+        """调用 draw_shadow 绘制阴影"""
         painter = QPainter(self)
-        self.draw_shadow(painter)
+        # 调用 draw_shadow
+        self.__draw_shadow(painter)
         painter.setPen(Qt.NoPen)
+        # 窗口本体是白色
         painter.setBrush(Qt.white)
-        painter.drawRect(QRect(SHADOW_WIDTH, SHADOW_WIDTH, self.width() - 2 * SHADOW_WIDTH,
-                               self.height() - 2 * SHADOW_WIDTH))
+        painter.drawRect(QRect(SHADOW_SIZE, SHADOW_SIZE, self.width() - 2 * SHADOW_SIZE,
+                               self.height() - 2 * SHADOW_SIZE))
+
+
+class BaseWindow(QWidget):
+    def __init__(self):
+        super(BaseWindow, self).__init__()
+        self.__window = RawWindow(self)
+
+    def show(self) -> None:
+        """重写，显示真正的窗口"""
+        self.__window.show()
+
+    def setWindowTitle(self, a0: str) -> None:
+        """重写，设置真正窗口的标题，只会显示在任务栏"""
+        self.__window.setWindowTitle(a0)
+
+    def setWindowIcon(self, icon: QtGui.QIcon) -> None:
+        """重写，设置真正窗口的图标，只会显示在任务栏"""
+        self.__window.setWindowIcon(icon)
 
 
 if __name__ == '__main__':
+    """测试"""
+    # 解决 Windows 下任务栏图标不显示的问题
+    my_id = u'syllabus_export.1.0'  # arbitrary string
+    windll.shell32.SetCurrentProcessExplicitAppUserModelID(my_id)
+
     app = QApplication(sys.argv)
+    app.setWindowIcon(QIcon('./images/icon/logo.png'))
     window = BaseWindow()
+    window.setWindowIcon(QIcon('./images/icon/logo.png'))
+    window.setWindowTitle('Just A Test')
     window.show()
     sys.exit(app.exec_())
